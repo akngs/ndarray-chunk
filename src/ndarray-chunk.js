9eeb95e27098d675241ab528cfbe05dccf2d15b8
akngs <jania902@gmail.com>
2016-01-25 20:06:50 +0900

    Add more tests; Handle plain JS array

--- src/ndarray-chunk.js
+++ src/ndarray-chunk.js
@@ -1,41 +1,45 @@
+import pack from 'ndarray-pack';
+
 function chunk(array, shape) {
-  if (array.shape.length !== shape.length) throw new Error('Shape mismatch')
+  array = array.shape ? array : pack(array);
 
-  let rank = shape.length
+  if (array.shape.length !== shape.length) throw new Error('Shape mismatch');
 
-  let remainders = []
-  let nChunks = []
+  let rank = shape.length;
+
+  let remainders = [];
+  let nChunks = [];
   for (let i = 0; i < rank; ++i) {
-    remainders.push(array.shape[i] % shape[i])
-    nChunks.push(Math.floor(array.shape[i] / shape[i]) + !!remainders[i])
+    remainders.push(array.shape[i] % shape[i]);
+    nChunks.push(Math.floor(array.shape[i] / shape[i]) + !!remainders[i]);
   }
 
-  let chunks = []
-  let hiCoor = []
-  let loCoor = []
-  let curChunks = chunks
+  let chunks = [];
+  let hiCoor = [];
+  let loCoor = [];
+  let curChunks = chunks;
 
   // TODO: Generalize to n-dimensional chunking and remove dups
   if (rank === 2) {
     for (let j = 0; j < nChunks[0]; ++j) {
-      curChunks = chunks[j] = []
-      hiCoor[0] = Math.min(j * shape[0] + shape[0], array.shape[0])
-      loCoor[0] = j * shape[0]
+      curChunks = chunks[j] = [];
+      hiCoor[0] = Math.min(j * shape[0] + shape[0], array.shape[0]);
+      loCoor[0] = j * shape[0];
 
       for (let i = 0; i < nChunks[1]; ++i) {
-        hiCoor[1] = Math.min(i * shape[1] + shape[1], array.shape[1])
-        loCoor[1] = i * shape[1]
-        curChunks[i] = array.hi(...hiCoor).lo(...loCoor)
+        hiCoor[1] = Math.min(i * shape[1] + shape[1], array.shape[1]);
+        loCoor[1] = i * shape[1];
+        curChunks[i] = array.hi(...hiCoor).lo(...loCoor);
       }
     }
   } else {
     for (let i = 0; i < nChunks[0]; ++i) {
-      hiCoor[0] = Math.min(i * shape[0] + shape[0], array.shape[0])
-      loCoor[0] = i * shape[0]
-      curChunks[i] = array.hi(...hiCoor).lo(...loCoor)
+      hiCoor[0] = Math.min(i * shape[0] + shape[0], array.shape[0]);
+      loCoor[0] = i * shape[0];
+      curChunks[i] = array.hi(...hiCoor).lo(...loCoor);
     }
   }
-  return chunks
+  return chunks;
 }
 
 export {
--- test/unit/ndarray-chunk.js
+++ test/unit/ndarray-chunk.js
@@ -12,6 +12,7 @@ describe('1D chunking', () => {
     expect(unpack(actual[0])).to.deep.equal([1, 2, 3]);
     expect(unpack(actual[1])).to.deep.equal([4, 5, 6]);
   });
+
   it('should deal with remainder', () => {
     let array = pack([1, 2, 3, 4, 5, 6]);
     let actual = chunk(array, [4]);
@@ -20,6 +21,7 @@ describe('1D chunking', () => {
     expect(unpack(actual[0])).to.deep.equal([1, 2, 3, 4]);
     expect(unpack(actual[1])).to.deep.equal([5, 6]);
   });
+
   it('should return empty array if the original array is empty', () => {
     expect(chunk(pack([]), [1]).length).to.equal(0);
   });
@@ -38,13 +40,26 @@ describe('2D chunking', () => {
     let actual = chunk(array, [2, 2]);
     expect(actual.length).to.equal(3);
     expect(actual[0].length).to.equal(3);
+
     expect(unpack(actual[0][0])).to.deep.equal([[1, 2], [6, 7]]);
+    expect(unpack(actual[0][1])).to.deep.equal([[3, 4], [8, 9]]);
+    expect(unpack(actual[0][2])).to.deep.equal([[5], [10]]);
+
+    expect(unpack(actual[1][0])).to.deep.equal([[11, 12], [16, 17]]);
     expect(unpack(actual[1][1])).to.deep.equal([[13, 14], [18, 19]]);
+    expect(unpack(actual[1][2])).to.deep.equal([[15], [20]]);
+
+    expect(unpack(actual[2][0])).to.deep.equal([[21, 22]]);
+    expect(unpack(actual[2][1])).to.deep.equal([[23, 24]]);
     expect(unpack(actual[2][2])).to.deep.equal([[25]]);
   });
 });
 
 describe('Parameter checking', () => {
+  it('should pack plain JS array', () => {
+    expect(chunk([1, 2, 3], [2]).length).to.equal(2);
+  });
+
   it('should check rank', () => {
     let array = pack([[1, 2], [3, 4]]);
     expect(() => chunk(array, [2])).to.throw(/Shape mismatch/);
